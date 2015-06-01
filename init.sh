df431cda1002b774052f2f68a9cf46f850ddce29
Daniel Levenson <dleve123@gmail.com>
2015-05-31 23:03:13 -0400

    Add comment for bash_profile to init.sh

--- init.sh
+++ init.sh
@@ -45,6 +45,7 @@ if [ "$bashgrep" != "source ~/dotfiles/bashrc" ]; then
   echo "source ~/dotfiles/bashrc" >> ~/.bashrc
 fi
 
+# bash_profile
 bashgrep=$(grep "source ~/dotfiles/bash_profile" ~/.bash_profile)
 if [ "$bashgrep" != "source ~/dotfiles/bash_profile" ]; then
   echo "source ~/dotfiles/bash_profile" >> ~/.bash_profile
