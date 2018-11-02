0b28ef4cc3cb02422cdcaea348e0eb8adf0f6fe2
Daniel Levenson <dleve123@gmail.com>
2018-11-02 01:34:01 -0400

    Cleanup whitespace emitted due to dirty git asterisk
    Previously, if the `git status` was clean, there would be an extra
    whitespace before the branch name in the Bash prompt. This is because the
    whitespace was hard-coded in the PS1. This change emits a whitespace
    after the asterisk only if the asterisk is present, keeping things
    clean.

--- prompt_config.sh
+++ prompt_config.sh
@@ -3,7 +3,7 @@ function parse_git_branch () {
 }
 
 function parse_git_dirty () {
-  [[ -z $(git status --porcelain 2> /dev/null) ]] || echo "*"
+  [[ -z $(git status --porcelain 2> /dev/null) ]] || echo " *"
 }
 
 CYAN="\[\033[0;36m\]"
@@ -12,4 +12,4 @@ GREEN="\[\033[0;32m\]"
 RED="\[\033[0;31m\]"
 NO_COLOUR="\[\033[0m\]"
 
-PS1="$GREEN\u@\h:$CYAN\w $RED\$(parse_git_dirty)$YELLOW\$(parse_git_branch)$NO_COLOUR\n\$ "
+PS1="$GREEN\u@\h:$CYAN\w$RED\$(parse_git_dirty)$YELLOW\$(parse_git_branch)$NO_COLOUR\n\$ "
