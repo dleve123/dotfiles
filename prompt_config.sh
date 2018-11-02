c0279342dccfefcf541abeef8d95d67ade3b2828
Daniel Levenson <dleve123@gmail.com>
2018-11-02 01:13:21 -0400

    Display asterisk (*) in Bash prompt if in dirty Git repo
    * Display before the branch name as occasionally branch names are very
    long, and placing after the branch name would go unnoticed.
    * Use `git status --porcelain` as it's recommended for scripting as it will
    stay constant over Git versions.
    * Inspired by https://thoughtbot.com/upcase/videos/git-getting-to-confident

--- prompt_config.sh
+++ prompt_config.sh
@@ -2,9 +2,14 @@ function parse_git_branch () {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
 
+function parse_git_dirty () {
+  [[ -z $(git status --porcelain 2> /dev/null) ]] || echo "*"
+}
+
 CYAN="\[\033[0;36m\]"
 YELLOW="\[\033[0;33m\]"
 GREEN="\[\033[0;32m\]"
+RED="\[\033[0;31m\]"
 NO_COLOUR="\[\033[0m\]"
 
-PS1="$GREEN\u@\h:$CYAN\w$YELLOW\$(parse_git_branch)$NO_COLOUR\n\$ "
+PS1="$GREEN\u@\h:$CYAN\w $RED\$(parse_git_dirty)$YELLOW\$(parse_git_branch)$NO_COLOUR\n\$ "
